1e38bf682fb87dd489fc5d321d811dc1caacccae
Erick Passos <erickcassianopassos@gmail.com>
2022-01-28 19:48:56 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,9 +7,9 @@ template<typename T_container, typename T = typename enable_if<!is_same<T_contai
 void dbg_out() { cerr << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
 #ifdef LOCAL
-#define DBG(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
-#define DBG(...)
+#define dbg(...)
 #endif
 
 // #define int long long
